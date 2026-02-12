710a7da21e334ab26272c6767ce0556bee68031e
Darshil Sharma <darshil.ips@gmail.com>
2026-02-12 13:05:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter.ttk import Label
+
+window = Tk()
+window.title("miles to Km converter")
+window.config(padx=50,pady=50)
+
+
+def miles_to_km():
+    miles =float(miles_input.get())
+    km = miles * 1.609
+    Kilometer_result_label.config(text=f"{km}")
+
+
+miles_input = Entry(width=7)
+miles_input.grid(row=0,column=1)
+
+
+miles_label= Label(text="M")
+miles_label.grid(row=0,column=2)
+
+is_equal_label = Label(text="is equal tp ")
+is_equal_label.grid(row=1,column=0)
+
+
+
+Kilometer_result_label = Label(text="0")
+Kilometer_result_label.grid(row=1,column=1)
+
+kilometer_label = Label(text="km")
+kilometer_label.grid(row=1, column=2)
+
+
+calculate_button = Button (text="Calculate",command=miles_to_km)
+calculate_button.grid(row=2,column=1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
